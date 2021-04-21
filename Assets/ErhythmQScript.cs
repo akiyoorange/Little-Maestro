2349b81be0243d2b51a311d2ef98b958380cc4cf
akiyoorange <79643871+akiyoorange@users.noreply.github.com>
2021-04-21 14:53:23 +0900

    Initial commit

---
+++ Assets/ErhythmQScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ErhythmQScript : MonoBehaviour
+{
+   　// Imageコンポーネントを格納する変数
+    private Image m_Image;
+    // 問題のスプライトオブジェクトを格納する配列
+    public Sprite[] m_Sprite;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Imageコンポーネントを取得して変数 m_Image に格納
+        m_Image = GetComponent<Image>();
+
+        // 一問目のスプライトをランダムに表示
+        //　m_Sprite[]に格納した問題のスプライトをRandomで割り当て）
+        m_Image.sprite = m_Sprite[Random.Range(0, 9)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スペースキーが押された場合
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+                // 二問目以降の問題スプライトの変更
+                m_Image.sprite = m_Sprite[Random.Range(0,9)];  
+        }
+    }
+}
---
+++ Assets/Home/SettingButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingButtonScript : MonoBehaviour
+{
+    public GameObject SettingPanel;
+    public void OnClickStartButton()
+    {
+        SettingPanel.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
