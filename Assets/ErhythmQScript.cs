c99287a4e3b433a2e72388b53c247cf87c0866fd
akiyoorange <79643871+akiyoorange@users.noreply.github.com>
2021-04-22 22:35:01 +0900

    正誤判定

--- Assets/ErhythmQScript.cs
+++ Assets/ErhythmQScript.cs
@@ -9,13 +9,13 @@ public class ErhythmQScript : MonoBehaviour
     private Image m_Image;
     // 問題のスプライトオブジェクトを格納する配列
     public Sprite[] m_Sprite;
-  
+
+    public GameObject RightPanel;
+    public GameObject WrongPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        //Answer1~4のボタンを取得
-        GameObject[] Answers = GameObject.FindGameObjectsWithTag("AnswerButton");
-
         // Imageコンポーネントを取得して変数 m_Image に格納
         m_Image = GetComponent<Image>();
 
@@ -27,21 +27,76 @@ public class ErhythmQScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool answer = true;
+        
+        
+      
+    }
 
-        if (m_Image.sprite = m_Sprite[0])
+    // 左上
+    public void Button_1()
+    {
+        if (m_Image.sprite = m_Sprite[1|2|6])
         {
-
+            RightPanel.SetActive(true);
+            // 二問目以降の問題スプライトの変更
+            m_Image.sprite = m_Sprite[Random.Range(0, 9)];
         }
-
-
-        // スペースキーが押された場合
-        if (Input.GetKeyDown(KeyCode.Space))
+        else
         {
-                // 二問目以降の問題スプライトの変更
-                m_Image.sprite = m_Sprite[Random.Range(0,9)];  
+            WrongPanel.SetActive(true);
+            // 二問目以降の問題スプライトの変更
+            m_Image.sprite = m_Sprite[Random.Range(0, 9)];
         }
+    }
 
-       
+    // 右上
+    public void Button_2()
+    {
+        if (m_Image.sprite = m_Sprite[3|5])
+        {
+            RightPanel.SetActive(true);
+            // 二問目以降の問題スプライトの変更
+            m_Image.sprite = m_Sprite[Random.Range(0, 9)];
+        }
+        else
+        {
+            WrongPanel.SetActive(true);
+            // 二問目以降の問題スプライトの変更
+            m_Image.sprite = m_Sprite[Random.Range(0, 9)];
+        }
+    }
+
+    // 左下
+    public void Button_3()
+    {
+        if (m_Image.sprite = m_Sprite[0|7|9|10])
+        {
+            RightPanel.SetActive(true);
+            // 二問目以降の問題スプライトの変更
+            m_Image.sprite = m_Sprite[Random.Range(0, 9)];
+        }
+        else
+        {
+            WrongPanel.SetActive(true);
+            // 二問目以降の問題スプライトの変更
+            m_Image.sprite = m_Sprite[Random.Range(0, 9)];
+        }
+    }
+
+    // 右下
+    public void Button_4()
+    {
+        if (m_Image.sprite = m_Sprite[4|8])
+        {
+            RightPanel.SetActive(true);
+            // 二問目以降の問題スプライトの変更
+            m_Image.sprite = m_Sprite[Random.Range(0, 9)];
+        }
+        else
+        {
+            WrongPanel.SetActive(true);
+            // 二問目以降の問題スプライトの変更
+            m_Image.sprite = m_Sprite[Random.Range(0, 9)];
+        }
     }
 }
