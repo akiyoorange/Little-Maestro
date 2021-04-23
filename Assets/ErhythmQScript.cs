4e73d6d90b4767ba90d64084805b5cc5d15e9f83
akiyoorange <79643871+akiyoorange@users.noreply.github.com>
2021-04-23 18:19:19 +0900

    更新

--- Assets/ErhythmQScript.cs
+++ Assets/ErhythmQScript.cs
@@ -10,6 +10,7 @@ public class ErhythmQScript : MonoBehaviour
     // 問題のスプライトオブジェクトを格納する配列
     public Sprite[] m_Sprite;
 
+    //正解と不正解のPanel
     public GameObject RightPanel;
     public GameObject WrongPanel;
 
@@ -21,7 +22,7 @@ public class ErhythmQScript : MonoBehaviour
 
         // 一問目のスプライトをランダムに表示
         //　m_Sprite[]に格納した問題のスプライトをRandomで割り当て）
-        m_Image.sprite = m_Sprite[Random.Range(0, m_Sprite.Length-1)];
+        m_Image.sprite = m_Sprite[Random.Range(0, m_Sprite.Length - 1)];
     }
 
     // Update is called once per frame
@@ -37,12 +38,14 @@ public class ErhythmQScript : MonoBehaviour
     {
         if (m_Image.sprite = m_Sprite[1|2|6])
         {
+            //正解
             RightPanel.SetActive(true);
             // 二問目以降の問題スプライトの変更
             m_Image.sprite = m_Sprite[Random.Range(0, m_Sprite.Length - 1)];
         }
         else
         {
+            //不正解
             WrongPanel.SetActive(true);
             // 二問目以降の問題スプライトの変更
             m_Image.sprite = m_Sprite[Random.Range(0,m_Sprite.Length - 1)];
