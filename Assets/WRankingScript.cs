09682647d5cf8444416ba37c6ae14b6f9e69aa71
akiyoorange <79643871+akiyoorange@users.noreply.github.com>
2021-04-30 18:30:50 +0900

    更新

---
+++ Assets/WRankingScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WRankingScript : MonoBehaviour
+{
+    public Text WorldRankingtext;
+    public Text playerCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WorldRankingtext.text = QuickRanking.Instance.GetRankingByText();
+
+        QuickRanking.Instance.FetchPlayerCount(SetPlayerCounter);
+    }
+
+    void SetPlayerCounter()
+    {
+        WorldRankingtext.text = PlayerPrefs.GetInt("Weekly") + QuickRanking.Instance.PlayerCount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
